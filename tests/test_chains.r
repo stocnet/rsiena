4a620aeb63339f0bdd970b258b732f2a62bc6411
Kaladani <daniel.gotthardt@gmx.de>
2025-07-22 18:55:46 +0200

    Cleanup of sienaRIDynamics and test_chains.
    - Harmonizing position of curly brackets (could go the other direction but should be the same in one file)
    
    - defaults are now set sensibly and fortify internal function against bad use cases
    
    - reduce unnecessary repetition of code
    
    - unify language "microStep" -> "ministep"
    
    - Made informal tests more meaningful (checking data structure).

--- tests/test_chains.r
+++ tests/test_chains.r
@@ -21,8 +21,9 @@ ans <- siena07(
   returnChangeContributions = TRUE
 )
 
-length(ans$changeContributions)
+length(ans$changeContributions) # 500 as expected
 head(ans$changeContributions[[1]][[1]][[1]])
+dim(ans$changeContributions[[1]][[1]][[1]][[1]]) # 3 x 50 as expected
 
 # Test returnChangeContributions when setting msub = 0 and prevAns = ans, using batch mode
 
@@ -35,8 +36,9 @@ ans2 <- siena07(
   prevAns = ans,
   returnChangeContributions = TRUE)
 
-length(ans2$changeContributions)
-head(ans2$changeContributions[[1]][[1]][[1]])
+length(ans$changeContributions) # 500 chainsas expected
+head(ans$changeContributions[[1]][[1]][[1]])
+lapply(ans$changeContributions[[1]][[1]][[1]], dim) # 3 x 50 as expected
 
 # Test sienaRIDynamics ----
 
@@ -52,10 +54,10 @@ plot(RIDynamics1)
 mycontrols2 <- sienaAlgorithmCreate(nsub=2, n3=50, cond=FALSE)
 mynet2 <- sienaDependent(array(c(tmp3, tmp4), dim=c(32, 32, 2)))
 mydata2 <- sienaDataCreate(mynet2)
-mymoldel2 <- getEffects(mydata2)
-mymoldel2 <- includeEffects(mymoldel2, density, recip, transTies, nbrDist2)
+mymodel2 <- getEffects(mydata2)
+mymodel2 <- includeEffects(mymodel2, density, recip, transTies, nbrDist2)
 
-ans2 <- siena07(mycontrols2, data=mydata2, effects=mymoldel2, batch=TRUE)
+ans2 <- siena07(mycontrols2, data=mydata2, effects=mymodel2, batch=TRUE)
 
 RIDynamics2 <- sienaRIDynamics(mydata2, ans=ans2)
 RIDynamics2
@@ -66,16 +68,16 @@ plot(RIDynamics2)
 ### Don't use ans but previously estimated coefficients ----
 
 RIDynamics3 <- sienaRIDynamics(data=mydata2, theta=c(ans2$rate,ans2$theta),
-             algorithm=mycontrols2, effects=mymoldel2, intervalsPerPeriod=10)
+             algorithm=mycontrols2, effects=mymodel2, intervalsPerPeriod=10)
 RIDynamics3
 plot(RIDynamics3)
 # if not exported to namespace use plot.sienaRIDynamics(RIDynamics3)
 
 ## Conditional Estimation ----
 mycontrols3 <- sienaAlgorithmCreate(nsub=2, n3=50, cond=TRUE)
-ans3 <- siena07(mycontrols3, data=mydata2, effects=mymoldel2, batch=TRUE)
+ans3 <- siena07(mycontrols3, data=mydata2, effects=mymodel2, batch=TRUE)
 
-RIDynamics4 <- sienaRIDynamics(mydata2, ans=ans3, effects = mymoldel2)
+RIDynamics4 <- sienaRIDynamics(mydata2, ans=ans3, effects = mymodel2)
 RIDynamics4
 plot(RIDynamics4)
 # if not exported to namespace use plot.sienaRIDynamics(RIDynamics4)
@@ -90,17 +92,25 @@ mymodel3 <- includeEffects(mymodel3, density, recip, transTies, transTrip, nbrDi
 ans4 <- siena07(mycontrols3, data=mydata3, effects=mymodel3, batch=TRUE,
                 returnChangeContributions=TRUE)
 
-length(ans4$changeContributions) # 50 as expected
-length(ans4$changeContributions[[1]][[1]]) # 2 as expected
+length(ans4$changeContributions) # 50 chains as expected
+length(ans4$changeContributions[[1]][[1]]) # 2 periods as expected
+beh_steps <- sapply(ans4$changeContributions[[1]][[1]][[2]], function(ministep) attr(ministep, "networkName")) %in%  "mybeh"
+any(beh_steps) # TRUE as expected
 
-head(ans4$changeContributions[[1]][[1]][[2]])
+ministeps <- ans4$changeContributions[[1]][[1]][[1]]
+getDepvarName <- function(ministep) attr(ministep, "networkName")
+beh_steps <- Filter(function(ministep) getDepvarName(ministep)  == "mybeh", ans4$changeContributions[[1]][[1]][[1]]) # 1st period, 1st group, 1st chain, behavior steps
+lapply(beh_steps, dim) # 2 x 3 as expected
 RIDynamics5 <- sienaRIDynamics(mydata3, ans=ans4, depvar="mybeh",
                                useChangeContributions=TRUE)
 RIDynamics5
 
-head(ans4$changeContributions[[1]][[1]][[1]])
+net_steps <- Filter(function(ministep) getDepvarName(ministep)  == "mynet3", ans4$changeContributions[[1]][[1]][[1]]) # 1st period, 1st group, 1st chain, network steps
+lapply(net_steps, dim) # 5 x 50 as expected
+
 RIDynamics6 <- sienaRIDynamics(mydata3, ans=ans4, depvar="mynet3")
 RIDynamics6
 
 RI6 <- sienaRI(mydata3, ans=ans4)
 plot(RIDynamics6, staticRI=RI6[[1]])
+
